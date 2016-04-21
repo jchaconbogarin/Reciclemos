505fe10b7815f972d1cbdb6232acc24fd2a2975c
jchaconbogarin <jchaconbogarin@gmail.com>
2016-04-20 19:26:59 -0700

    Progress added

--- app/src/main/java/itcr/reciclemos/MainActivity.java
+++ app/src/main/java/itcr/reciclemos/MainActivity.java
@@ -1,6 +1,8 @@
 package itcr.reciclemos;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Handler;
@@ -29,6 +31,8 @@ import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import itcr.reciclemos.gameutilities.Progress;
+
 public class MainActivity extends AppCompatActivity {
 
     //-- GUI Elements ---------------------
@@ -40,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Handler animationHandler;
     private Runnable animationRunnable;
+
+    private SharedPreferences progressData;
 
     Utilities toolBox = Utilities.getSingleton();
 
@@ -123,6 +129,12 @@ public class MainActivity extends AppCompatActivity {
         aboutBtn.setLayoutParams(params);
 
         animationHandler = new Handler();
+
+        progressData = getSharedPreferences(Progress.preferencesValue, Context.MODE_PRIVATE);
+
+        boolean hasSeenInformation = progressData.getBoolean(Progress.information, false);
+        boolean hasCompletedHouse = progressData.getBoolean(Progress.house, false);
+        boolean hasCompletedLake = progressData.getBoolean(Progress.lake, false);
     }
 
     @Override
